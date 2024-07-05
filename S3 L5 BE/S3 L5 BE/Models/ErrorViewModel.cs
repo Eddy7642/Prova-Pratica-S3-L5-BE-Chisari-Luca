67c05e377c0a9c74f141b75cdb427e998204d97a
Luca Chisari <75541078+Eddy7642@users.noreply.github.com>
2024-07-05 14:28:57 +0200

    Commit 1

---
+++ S3 L5 BE/S3 L5 BE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S3_L5_BE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
